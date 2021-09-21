4a9cd2a72b4164d67142d6b0c5768df5bbe7c659
Oliver Gröning <o.groening@gmx.de>
2021-09-21 17:52:23 +0200

    Clean up 2

--- target/wasm32-unknown-unknown/release/deps/wasm.d
+++
@@ -1,5 +0,0 @@
-/home/olli/Code/rust/wasm/target/wasm32-unknown-unknown/release/deps/wasm.wasm: src/lib.rs
-
-/home/olli/Code/rust/wasm/target/wasm32-unknown-unknown/release/deps/wasm.d: src/lib.rs
-
-src/lib.rs:
--- target/wasm32-unknown-unknown/release/wasm.d
+++
@@ -1,1 +0,0 @@
-/home/olli/Code/rust/wasm/target/wasm32-unknown-unknown/release/wasm.wasm: /home/olli/Code/rust/wasm/src/lib.rs
