face5cc9390e99dacfdaaebc05ab578681c1457b
Oliver Gröning <o.groening@gmx.de>
2021-09-21 11:38:52 +0200

    Include target

---
+++ target/wasm32-unknown-unknown/release/deps/wasm.d
@@ -0,0 +1,5 @@
+/home/olli/Code/rust/wasm/target/wasm32-unknown-unknown/release/deps/wasm.wasm: src/lib.rs
+
+/home/olli/Code/rust/wasm/target/wasm32-unknown-unknown/release/deps/wasm.d: src/lib.rs
+
+src/lib.rs:
---
+++ target/wasm32-unknown-unknown/release/wasm.d
@@ -0,0 +1,1 @@
+/home/olli/Code/rust/wasm/target/wasm32-unknown-unknown/release/wasm.wasm: /home/olli/Code/rust/wasm/src/lib.rs
